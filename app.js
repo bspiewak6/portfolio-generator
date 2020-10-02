01aac75fff87245717c0d4f6e64a745f30d9d337
Spiewak <bspiewak6@gmail.com>
2020-10-02 18:49:58 -0400

    finish up site to now write the full index.html and destructure code

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+// const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site');
 const generatePage = require('./src/page-template');
 const inquirer = require('inquirer');
 
@@ -121,22 +122,48 @@ Add a New Project
             default: false
         }
     ])
-                    .then(projectData => {
-                        portfolioData.projects.push(projectData);
-                        if (projectData.confirmAddProject) {
-                          return promptProject(portfolioData);
-                        } else {
-                          return portfolioData;
-                        }
-                      });
-    }
-    
-    promptUser()
+        .then(projectData => {
+            portfolioData.projects.push(projectData);
+            if (projectData.confirmAddProject) {
+                return promptProject(portfolioData);
+            } else {
+                return portfolioData;
+            }
+        });
+}
+
+promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile('./index.html', pageHTML, err => {
-            if (err) throw new Error (err);
-        });
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileReponse => {
+        console.log(copyFileReponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
+
+
+    // fs.writeFile('./dist/index.html', pageHTML, err => {
+    //     if (err) {
+    //       console.log(err);
+    //       return;
+    //     }
+    //     console.log('Page created! Check out index.html in this directory to see it!');
+
+    //     fs.copyFile('./src/style.css', './dist/style.css', err => {
+    //       if (err) {
+    //         console.log(err);
+    //         return;
+    //       }
+    //       console.log('Style sheet copied successfully!');
+    //     });
+    //   });
